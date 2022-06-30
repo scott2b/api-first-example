a9f1b4e4d4e764eea6685d2e32096014e4ea3136
scott2b <@scott2b>
2022-06-30 15:12:28 -0500

    cleanup backend

--- src/common/backends.py
+++ src/common/backends.py
@@ -1,60 +1,6 @@
-import base64
-import copy
 import datetime
 from starlette.authentication import AuthenticationBackend, AuthCredentials
-from starlette.authentication import SimpleUser
-from starlette.exceptions import HTTPException
-from starlette.middleware.base import BaseHTTPMiddleware
 from .models import User, OAuth2Token
-
-
-# from cryptography.fernet import Fernet
-# from common.config import settings
-
-# csrf_secret = settings.CSRF_KEY
-
-# def fernet(key: str) -> Fernet:
-#    key = key + "=" * (len(key) % 4)
-#    _key = base64.urlsafe_b64encode(base64.urlsafe_b64decode(key))
-#    return Fernet(_key)
-#
-# def encrypt(data: str) -> bytes:
-#    return fernet(csrf_secret).encrypt(data.encode())
-#
-# def decrypt(data: bytes) -> bytes:
-#    return fernet(csrf_secret).decrypt(data)
-
-
-# class CSRFMiddleware(BaseHTTPMiddleware):
-#
-#    async def dispatch(self, request, call_next):
-#        """I have modeled this after Django's approach of putting the CSRF token in the
-#        headers (see: https://docs.djangoproject.com/en/4.0/ref/csrf/#ajax) mainly
-#        because Starlette does not let us access the request body in middleware
-#        (see: https://github.com/encode/starlette/issues/495) but also because it is
-#        better not to anyway. This is currently inconsistent with our forms handling,
-#        where the CSRF token is included as hidden data in a form.
-#        """
-#        print("CSRF MIDDLEWARE", request.method)
-#        print(request.headers)
-#        print(request.scope)
-#        print(dir(request.scope["auth"]))
-#        print(request.scope["auth"].scopes)
-#        print(dir(request))
-#        print(request.base_url)
-#        print(request.url)
-#        #if request.method == "POST" and "application/json" in request.headers["content-type"].split(";"):
-#        #    token = request.headers.get("x-csrftoken")
-#        #    print("CSRF TOKEN", token.encode())
-#        #    user_id = decrypt(token.encode())
-#        #    print("USER ID:", user_id)
-#        #request.scope["csrftoken"] = encrypt(str(request.user.id)).decode()
-#        #print("GETTING RESPONSE")
-#        response = await call_next(request)
-#        print(request.headers)
-#        #print("CSRF IN SCOPE:", request.scope["csrftoken"])
-#        print("RETURNING RESPONSE")
-#        return response
 
 
 class SessionAuthBackend(AuthenticationBackend):
